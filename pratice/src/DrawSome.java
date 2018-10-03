172dbf34c82babc2a4a0c7e2f8d1f6067a1817cb
Kot <kotvnn@msn.com>
2018-10-03 23:42:07 +0800

    Done

---
+++ pratice/src/DrawSome.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DrawSome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào theo số thứ tự sau để vẽ hình");
+        System.out.println("1. Vẽ hình chữ nhật");
+        System.out.println("2. Vẽ hình tam giác vuông bottom-left");
+        System.out.println("3. Vẽ hình tam giác vuông top-left");
+        int stt = sc.nextInt();
+        switch (stt) {
+            case 1:
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                }
+                break;
+            case 2:
+                for (int i = 1; i < 8; i++) {
+                    for (int j = 1; j < i; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                }
+                break;
+            case 3:
+                for (int i = 6; i > 0; i--) {
+                    for (int j = i; j > 0; j--) {
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                }
+        }
+    }
+}
